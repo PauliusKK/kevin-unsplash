37ef97021e385f81e0e117cf61209b876b6d075b
PauliusKK <17347293+PauliusKK@users.noreply.github.com>
2021-02-28 02:01:34 +0200

    Create App, do everything in app.js just to test if everything works, add masonry package, because got exhausted trying to implement my own solution, as it gets buggy, hard to maintain and complicated. Test done.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Masonry from 'react-masonry-css'
 import './App.css';
 
+const ACCESS_KEY = 'OSt_9Mgrjej5AEkCijPYhOB8nupoeTVQ9FH2MJjuvr8';
+
+const unsplash = axios.create({
+  baseURL: 'https://api.unsplash.com',
+});
+
+unsplash.defaults.headers.common['Authorization'] = `Client-ID ${ACCESS_KEY}`;
+
 function App() {
+  const [photos, setPhotos] = useState([]);
+
+  if (photos.length <= 1) {
+    unsplash.get('/photos', {
+      params: {
+        per_page: 30,
+      }
+    })
+    .then((response) => {
+      console.log(response);
+      const photos = response.data.map((photo) => {
+        const { id, urls } = photo;
+        const { regular: source } = urls;
+  
+        return {
+          id,
+          source,
+        }
+      });
+      console.log('set photos');
+      setPhotos(photos);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
+
+  const breakpointColumns = {
+    default: 5,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Masonry
+        breakpointCols={breakpointColumns}
+        className="masonry-container"
+        columnClassName="masonry-column"
+      >
+        {photos.map((photo) => {
+          return (
+            <div key={photo.id} className="photo-box">
+              <img src={photo.source} />
+            </div>
+          )
+        })}
+      </Masonry>
     </div>
   );
 }
---
+++ src/todo.md
@@ -0,0 +1,4 @@
+// TODO
+
+-- when you scroll down, load more images. Google how to do that, but I think measuring the body height, and using an event handler to catch when user goes to the end is what would work.
+
