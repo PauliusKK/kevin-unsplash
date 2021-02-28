992229a1de8808a79b040f1e982d16bae08047a8
PauliusKK <17347293+PauliusKK@users.noreply.github.com>
2021-02-28 03:12:38 +0200

    Add typescript, components, changing structure and making app better

---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './scss/navigation.scss';
+
+// export interface Navigation {
+//   children: React.ReactNode;
+//   className?: string;
+// }
+
+export const Navigation = () => (
+  <div className="navigation">
+    {'Heres our navigation Logo And button centered And whole navigation display: fixed or position: fixed'}
+  </div>
+);
